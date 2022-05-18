f12b7fba284e5f401738190c2e5714a4e4456d99
reuben <creubend@gmail.com>
2022-05-18 10:26:37 -0600

    test: reformat test to expect standard json

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       resources :merchants, only: [:index, :show] do 
-        resources :items
+        resources :items, only: [:index], controller: 'merchant_items'
       end
     end
   end
--- spec/requests/api/v1/merchant_items_request_spec.rb
+++ spec/requests/api/v1/merchant_items_request_spec.rb
@@ -12,17 +12,21 @@ RSpec.describe "Merchant Items API " do
         expect(items[:data].count).to eq(5)
 
         items[:data].each do |item|
+            
             expect(item).to have_key(:id)
             expect(item[:id]).to be_a(String)
-
+            
             expect(item[:attributes]).to have_key(:name)
             expect(item[:attributes][:name]).to be_a(String)
             
             expect(item[:attributes]).to have_key(:description)
             expect(item[:attributes][:description]).to be_a(String)
-
+            
             expect(item[:attributes]).to have_key(:unit_price)
             expect(item[:attributes][:unit_price]).to be_a(Float)
+            
+            expect(item[:attributes]).to have_key(:merchant_id) 
+            expect(item[:attributes][:merchant_id]).to be_an(Integer)
         end
     end
 end 
