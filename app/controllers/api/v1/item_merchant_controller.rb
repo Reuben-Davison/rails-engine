398ec1511ad548237b3e89bb858bc983ca1e57fa
reuben <creubend@gmail.com>
2022-05-19 08:26:14 -0600

    feat: Item merchant info passes rspec tests and postman

--- app/controllers/api/v1/item_merchant_controller.rb
+++ app/controllers/api/v1/item_merchant_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::ItemMerchantController <ApplicationController 
     def index 
-        binding.pry
         render json: MerchantSerializer.new(Item.find(params[:item_id]).merchant)
     end
 end
--- spec/requests/api/v1/item_merchant_request_spec.rb
+++ spec/requests/api/v1/item_merchant_request_spec.rb
@@ -4,11 +4,15 @@ RSpec.describe "Item merchant API " do
     it "gets the info of a item's merchant" do 
         non_merchants = create_list(:merchant, 5)
         item = create(:item)
-        binding.pry
         get "/api/v1/items/#{item.id}/merchant"
         expect(response).to be_successful
 
         merchant_info = JSON.parse(response.body, symbolize_names: true)
+        expect(merchant_info[:data][:attributes][:name]).to eq(item.merchant.name)
+        
+        non_merchants.each do |merchant|
+            expect(merchant.name).to_not be_in(merchant_info)
+        end
 
     
     end
